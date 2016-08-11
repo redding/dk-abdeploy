f0ee6e7a5731f06d02b849b6ca23476b6f744a58
Kelly Redding <kelly@kellyredding.com>
2016-08-11 12:18:47 -0500

    (hotfix) require in the new Cleanup task for convenience
    This was missed in #10 and should have been done in that effort.

--- lib/dk-abdeploy.rb
+++ lib/dk-abdeploy.rb
@@ -2,6 +2,7 @@ require "dk-abdeploy/version"
 require "dk-abdeploy/constants"
 
 # require the tasks for convenience
+require 'dk-abdeploy/cleanup'
 require 'dk-abdeploy/link'
 require 'dk-abdeploy/update'
 require 'dk-abdeploy/setup'
